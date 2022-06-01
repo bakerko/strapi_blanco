8f9c0537dff0b489ed6925e2a783e4644505b7ac
Bakerko <your_email_address@example.comua.nikutin@gmail.com>
2022-06-01 16:08:09 +0300

    database ssl connection

--- config/database.js
+++ config/database.js
@@ -5,7 +5,13 @@ module.exports = ({ env }) => ({
     client: 'sqlite',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
     useNullAsDefault: true,
+    options: {
+      ssl: env.bool('DATABASE_SSL', false),
+    },
   },
 });
